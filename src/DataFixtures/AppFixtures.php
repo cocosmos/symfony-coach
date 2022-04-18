f23b5eb51a0532f47b2ca415276d8509f3e87a70
Cocosmos <mipam.guillot@crea-inseec.com>
2022-04-18 21:02:14 +0200

    add fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Priority;
+use App\Entity\Status;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,15 +11,31 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $priorityNames =["On occasion, come and give us a feedback", "Small request that does not rush too much", "We need help !", "It blocks us completely !"];
+        $statusNames =["We think…","We arrive", "Figure it out", "Declined", "Solved"];
 
-//        for ($i = 0; $i < 20; $i++) {
-//            $product = new Product();
-//            $product->setName('product '.$i);
-//            $product->setPrice(mt_rand(10, 100));
-//            $manager->persist($product);
-//        }
+
+        foreach ( $priorityNames as $key => $priorityName){
+
+
+            $priority = new Priority();
+            $priority->setName($priorityName);
+            $priority->setWeight($key+1);
+            $manager->persist($priority);
+        }
+        foreach ($statusNames as $statusName){
+            $status = new Status();
+            $status->setName($statusName);
+            if ($statusName  == "Declined" || $statusName =="Solved"){
+                $status->setIsArchived(true);
+            }else{
+                $status->setIsArchived(false);
+            }
+            $manager->persist($status);
+
+        }
+
+
 
         $manager->flush();
     }
